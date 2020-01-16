e1b0056d7eb8809e5210e2c903d5cdc6ede25963
Francisco Altoe <franciscoda@outlook.com>
2020-01-16 18:12:54 -0300

    install distro pakcages in install.sh

--- install.sh
+++ install.sh
@@ -14,3 +14,21 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # install vundle plugins
 vim +PluginInstall +qall
+
+DISTRO_ID=$(bash -c 'source /etc/os-release && echo $ID')
+
+# install distro packages
+
+case $DISTRO_ID in
+arch)
+	sudo pacman -S bash-completion
+	if [ -z "$SSH_CLIENT" ] ; then
+		# install packages that are only useful in a graphic setup
+		sudo pacman -S adobe-source-code-pro-fonts
+	fi
+	;;
+*)
+	echo "Could not install distro-specific packages for distro id: $DISTRO_ID"
+	;;
+esac
+
