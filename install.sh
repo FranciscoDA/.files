a375542c74cefaf463b39023c1c049fc13518e71
Francisco Altoe <franciscoda@outlook.com>
2020-02-04 22:38:38 -0300

    update installation script to not reinstall packages and vim colorschemes

--- install.sh
+++ install.sh
@@ -9,32 +9,45 @@ cp "$PROJECT_ROOT/vimrc" ~/.vimrc
 cat "$PROJECT_ROOT/Xdefaults" >> ~/.Xdefaults
 cp "$PROJECT_ROOT/bashrc" ~/.bashrc
 
-mkdir -p ~/.vim/colors
-
 # install badwolf vim colorscheme
+VIM_COLOR_DIR=$(readlink -f ~/.vim/colors)
+mkdir -p "$VIM_COLOR_DIR"
 for file in badwolf.vim goodwolf.vim ; do
-	curl "https://raw.githubusercontent.com/sjl/badwolf/master/colors/$file" -o ~/.vim/colors/$file
+	if ! [ -a "$VIM_COLOR_DIR/$file" ]; then
+		curl "https://raw.githubusercontent.com/sjl/badwolf/master/colors/$file" -o "$VIM_COLOR_DIR/$file"
+	fi
 done
 
 
 # install vim vundle
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+if ! [ -d ~/.vim/bundle/Vundle.vim ]; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
 # install vundle plugins
 vim +PluginInstall +qall
 
-DISTRO_ID=$(bash -c 'source /etc/os-release && echo $ID')
-
 # install distro packages
+DISTRO_ID=$(bash -c 'source /etc/os-release && echo $ID')
+PACKAGES_TO_INSTALL=()
+
+function arch_add_missing_pkgs() {
+	for PKG in $@; do
+		if ! pacman -Qi $PKG >/dev/null 2>/dev/null ; then
+			PACKAGES_TO_INSTALL+=("$PKG")
+		fi
+	done
+}
 
 case $DISTRO_ID in
 arch)
-	sudo pacman -S bash-completion
-	if [ -z "$SSH_CLIENT" ] ; then
+	arch_add_missing_pkgs bash-completion
+	if [ -z "$SSH_CLIENT" ]; then
 		# install packages that are only useful in a graphic setup
-		sudo pacman -S adobe-source-code-pro-fonts powerline-fonts
+		arch_add_missing_pkgs adobe-source-code-pro-fonts powerline-fonts
+	fi
+	if [ ${#PACKAGES_TO_INSTALL[@]} -gt 0 ]; then
+		sudo pacman -S ${PACKAGES_TO_INSTALL[@]}
 	fi
 	;;
 *)
